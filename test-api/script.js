8b9e6fa3f53bc890f24acb2ab8fbb55460d25fbc
Misha Kessler <misha.kessler@gmail.com>
2019-06-07 16:42:24 -0400

    Cleaned unnecessary test/research files.

--- test-api/script.js
+++
@@ -1,9 +0,0 @@
-const DOMAIN = 'https://trends.google.com/trends/explore';
-const BASE_URL = `${DOMAIN}?`;
-
-
-const getKeyword = async () => {
-  const response = await get("trends.google.com/trends/explore?q=influenza&geo=US")
-  console.log(response);
-}
-console.log(getKeyword());
